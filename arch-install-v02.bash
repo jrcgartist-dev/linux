3a8ccf372f101321892581ed68a912de2d54cca0
Luiz Armando Pitondo Junior <57337956+jrcgartist-dev@users.noreply.github.com>
2021-04-22 16:45:02 -0300

    Update arch-install-v02.bash

--- arch-install-v02.bash
+++ arch-install-v02.bash
@@ -1,77 +1,38 @@
 #!/bin/bash
 
-bootstrapper_dialog() {
-    DIALOG_RESULT=$(dialog --clear --stdout --backtitle "Arch bootstrapper" --no-shadow "$@" 2>/dev/null)
-}
 
-#################
-#### Welcome ####
-#################
-bootstrapper_dialog --title "Welcome" --msgbox "Welcome to Arch Linux bootstrapper.\n" 6 60
-
-##############################
-#### UEFI / BIOS detection ###
-##############################
-
-partition_var=0
-
-if [[ $partition_var -eq 0 ]]; then
-    LMV_radio="on"
-    EXT4_radio="off"
-else
-    LMV_radio="off"
-    EXT4_radio="on"
-fi
-
-bootstrapper_dialog --title "LMV or EXT4" --radiolist "\nPress <Enter> to accept." 10 30 2 1 LMV "$LMV_radio" 2 EXT4 "$EXT4_radio"
-[[ $DIALOG_RESULT -eq 1 ]] && LMV=1 || LMV=0
-
-#################
-#### Prompts ####
-#################
-bootstrapper_dialog --title "Hostname" --inputbox "Please enter a name for this host.\n" 8 60
-hostname="$DIALOG_RESULT"
-
-#################
-#### User name ####
-#################
-bootstrapper_dialog --title "User name" --inputbox "Please enter a name for the USER.\n" 8 60
-user_name="$DIALOG_RESULT"
-
-##########################
-#### Password prompts ####
-##########################
-bootstrapper_dialog --title "Root password" --passwordbox "Please enter a strong password for the root user.\n" 8 60
-root_password="$DIALOG_RESULT"
-
-bootstrapper_dialog --title "User password" --passwordbox "Please enter a strong password for the user.\n" 8 60
-user_password="$DIALOG_RESULT"
-
-#################
-#### Warning ####
-#################
-bootstrapper_dialog --title "WARNING" --msgbox "This script will NUKE /dev/nvme0n1 from orbit.\nPress <Enter> to continue or <Esc> to cancel.\n" 6 60
-[[ $? -ne 0 ]] && (bootstrapper_dialog --title "Cancelled" --msgbox "Script was cancelled at your request." 5 40; exit 0)
-
-##########################
-#### reset the screen ####
-##########################
-reset
-
-
-DISK=/dev/nvme0n1
-DISK_EFI=/dev/nvme0n1p1
-DISK_MNT=/dev/nvme0n1p2
+LMV=1
+hostname=arch
+user_name=kobe24
+root_password=password
+user_password=password
+DISK=/dev/sda
+DISK_EFI=/dev/sda1
+DISK_MNT=/dev/sda2
 TIMEZONE=America/Sao_Paulo
 LANGUAGE=en_US.UTF-8
 GRUBDIR=/mnt/boot/EFI
+GNOME=1
+AUR_PACK=true
+PACKAGES_AUR_INSTALL=true
+PACKAGES_AUR_COMMAND=paru
 
+
+
+echo "Update System Clock"
+timedatectl set-ntp true
 
 #########################################
 #### Nuke and set up disk partitions ####
 #########################################
 echo "Zapping disk"
 sgdisk --zap-all $DISK
+
+swapoff --all
+umount --recursive /mnt
+lvremove --force system/root
+lvremove --force system/swap
+vgremove --force system
 
 echo "Creating EFI Partition"
 if [[ $LMV -eq 1 ]]; then
@@ -93,7 +54,7 @@ fi
 echo "Setting up LVM"
 pvcreate --dataalignment 1m "${DISK_MNT}"
 vgcreate vg00 "${DISK_MNT}"
-lvcreate -L 30G vg00 -n lv_root
+lvcreate -L 10G vg00 -n lv_root
 lvcreate -l +100%FREE vg00 -n lv_home
 modprobe dm_mod
 vgscan
@@ -133,10 +94,13 @@ echo "LANG=${LANGUAGE}" >> /etc/locale.conf
 echo "Setting time zone"
 ln -s /usr/share/zoneinfo/$TIMEZONE /etc/localtime
 timedatectl set-timezone $TIMEZONE
-hwclock --systohc
+hwclock --systohc --utc
+
+echo "Installing main packages"
+pacman --noconfirm --needed -Syu linux linux-firmware linux-headers
 
 echo "Installing network packages"
-pacman --noconfirm --needed -S networkmanager wpa_supplicant wireless_tools netctl dialog
+pacman --noconfirm --needed -Syu networkmanager wpa_supplicant wireless_tools netctl dialog
 
 echo "Setting network configuration"
 echo $hostname > /etc/hostname
@@ -144,23 +108,20 @@ echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 ${hostname}.localdomain ${hostname}" >> /etc/hosts
 
-echo "Installing main packages"
-pacman --noconfirm --needed -S linux linux-firmware linux-headers
-
 echo "Initramfs"
 mkinitcpio -P
 
 echo "Install CPU Microde files"
-pacman --noconfirm --needed -S amd-ucode
+pacman --noconfirm --needed -Syu amd-ucode
 
 echo "Installing LVM"
-pacman --noconfirm --needed -S lvm2
+pacman --noconfirm --needed -Syu lvm2
 
 echo "Installing Editor"
-pacman --noconfirm --needed -S nano
+pacman --noconfirm --needed -Syu nano
 
 echo "Installing file systems"
-pacman --noconfirm --needed -S btrfs-progs dosfstools exfatprogs e2fsprogs ntfs-3g xfsprogs
+pacman --noconfirm --needed -Syu btrfs-progs dosfstools exfatprogs e2fsprogs ntfs-3g xfsprogs
 
 echo "Generating initramfs"
 sed -i 's/^HOOKS.*/HOOKS="base udev autodetect modconf block lvm2 filesystems keyboard fsck"/' /etc/mkinitcpio.conf
@@ -177,33 +138,94 @@ echo "${user_name}:${user_password}" | chpasswd
 EDITOR=nano visudo
 
 echo "Installing Grub boot loader"
-pacman --noconfirm --needed -S grub efibootmgr dosfstools os-prober mtools
+pacman --noconfirm --needed -Syu grub efibootmgr dosfstools os-prober mtools
 grub-install --target=x86_64-efi --efi-directory=$GRUBDIR --bootloader-id=GRUB
 
 #sed -i 's|^GRUB_CMDLINE_LINUX_DEFAULT.*|GRUB_CMDLINE_LINUX_DEFAULT="nouveau.modeset=0"|' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
 
-echo "Windows Management"
-pacman -S --needed xorg-server xorg-xinit
-
-echo "Nvidia Driver"
-pacman -S --needed nvidia nvidia-utils nvidia-settings opencl-nvidia
-bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
-
-echo "Gnome"
-pacman -S --needed gnome gnome-tweaks
-systemctl enable gdm
+echo "Swap File"
+dd if=/dev/zero of=/swapfile bs=1M count=2048 status=progress
+chmod 600 /swapfile
+mkswap /swapfile
+cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | tee -a /etc/fstab
 
 echo "Enabling systemctls"
 systemctl enable NetworkManager
 systemctl enable systemd-timesyncd
-
 EOF
 
+
+#############################
+#### Installing a DE ####
+#############################
+if [[ $GNOME -eq 1 ]]; then
+
+arch-chroot /mnt /bin/bash <<EOF
+echo "Windows Management"
+pacman -Syu --needed xorg-server xorg-xinit
+
+echo "Nvidia Driver"
+pacman -Syu --needed nvidia nvidia-utils nvidia-settings opencl-nvidia
+nvidia-xconfig
+bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+
+echo "Gnome"
+pacman -Syu --needed gnome gnome-tweaks
+systemctl enable gdm
+EOF
+else
+arch-chroot /mnt /bin/bash <<EOF
+echo "Pass"
+EOF
+
+#############################
+#### Installing Paru ####
+#############################
+
+function packages_aur() {
+    print_step "packages_aur()"
+
+    if [ "$PACKAGES_AUR_INSTALL" == "true" ]; then
+        pacman -Syu --needed git
+
+        case "$PACKAGES_AUR_COMMAND" in
+            "yay" )
+                execute_aur "rm -rf /home/$user_name/.tmp/aur/$PACKAGES_AUR_COMMAND && mkdir -p /home/$user_name/.tmp/aur && cd /home/$user_name/.tmp/aur && git clone https://aur.archlinux.org/$PACKAGES_AUR_COMMAND.git && (cd $PACKAGES_AUR_COMMAND && makepkg -si --noconfirm) && rm -rf /home/$user_name/.tmp/aur/$PACKAGES_AUR_COMMAND"
+                ;;
+            "paru" | *)
+                execute_aur "rm -rf /home/$user_name/.tmp/aur/$PACKAGES_AUR_COMMAND && mkdir -p /home/$user_name/.tmp/aur && cd /home/$user_name/.tmp/aur && git clone https://aur.archlinux.org/$PACKAGES_AUR_COMMAND.git && (cd $PACKAGES_AUR_COMMAND && makepkg -si --noconfirm) && rm -rf /home/$user_name/.tmp/aur/$PACKAGES_AUR_COMMAND"
+                ;;
+        esac
+	echo "Pass"
+    fi
+}
+
+
+if [[ $AUR_PACK == "true" ]]; then
+
+arch-chroot /mnt /bin/bash <<EOF
+
+packages_aur
+
+EOF
+else
+arch-chroot /mnt /bin/bash <<EOF
+echo "Pass"
+EOF
+
+
+fi
+
+#################
+#### The end ####
+#################
+
+# https://github.com/picodotdev/alis/blob/master/alis-packages.sh
+
 printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
-
-
 
 
 
